9d5055b3d4e9b4cc5a6185d58c4be44f44fbe8fe
longdinho <haocute2912@gmail.com>
2025-06-30 15:26:11 +0700

    upadate1

--- Module_10/project1/src/main.jsx
+++ Module_10/project1/src/main.jsx
@@ -1,15 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App.jsx';
-import store from './redux/store.jsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <Provider>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>,
-)
+createRoot(document.getElementById("root")).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
